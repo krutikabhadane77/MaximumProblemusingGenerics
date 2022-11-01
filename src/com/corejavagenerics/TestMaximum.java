25c97a0a660b054316311fc029153047d92c5c9e
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-11-02 00:00:22 +0530

    Refactor: create Generic Class to take in 3 variables of Generic Type

--- src/com/corejavagenerics/TestMaximum.java
+++ src/com/corejavagenerics/TestMaximum.java
@@ -2,29 +2,25 @@
 //UC2 Given 3 Floats find the maximum
 //UC3 Given 3 Strings find the maximum
 //Refactor all the 3 to One Generic Method and find the maximum
+//Refactor-2 to create Generic Class to take in 3 variables of Generic Type
 package com.corejavagenerics;
 
 public class TestMaximum {
-    public static <T extends Comparable<T>> void getMax(T[] arr) {
-        T max = arr[0];
-        int position = 0;
-        for (int i = 0; i < arr.length; i++)
-        {
-            int a = arr[i].compareTo(max);
-            if(a > 0)
-            {
-                max = arr[i];
-                position = i;
-            }
-        }
-        System.out.println("Maximum element is " + max+" and its position is "+position);
+    public static <T extends Comparable<T>> T getMax(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
+        return max;
     }
     public static void main(String[] args) {
-        Integer[] integerArray = {10, 40, 20};
-        getMax(integerArray);
-        Float[] floatArray = {2.3f,4.2f,7.5f};
-        getMax(floatArray);
-        String[] stringArray = {"Apple", "Peach", "Banana"};
-        getMax(stringArray);
+        getMax(10,40,20);
+        System.out.println("Maximum integer is "+getMax(10,40,20));
+        getMax(2.3f,4.2f,7.5f);
+        System.out.println("Maximum float is "+getMax(2.3f,4.2f,7.5f));
+        getMax("Apple", "Peach", "Banana");
+        System.out.println("Maximum string is "+getMax("Apple", "Peach", "Banana"));
+
     }
 }
