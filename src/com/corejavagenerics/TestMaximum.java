20141c4b43e69bbb79fa21d18339b8756ed4e4c0
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-11-01 22:33:37 +0530

    Added: UC2 Given 3 floats find the maximum float

--- src/com/corejavagenerics/TestMaximum.java
+++ src/com/corejavagenerics/TestMaximum.java
@@ -1,10 +1,11 @@
 //UC1 Given 3 Integers find the maximum
+//UC2 Given 3 Floats find the maximum
 package com.corejavagenerics;
 
 public class TestMaximum {
-    public static void getMaximumInteger(Integer[] arr) {
+    public static void getMaximumFloat(Float[] arr) {
 
-        int max = arr[0];
+        Float max = arr[0];
         int position = 0;
         for (int i = 0; i < arr.length; i++) {
             int a = arr[i].compareTo(max);
@@ -16,8 +17,8 @@ public class TestMaximum {
         System.out.println("Maximum integer is " + max+" and its position is "+position);
     }
     public static void main(String[] args) {
-        Integer[] arr = {10, 40, 20};
-        getMaximumInteger(arr);
+        Float[] arr = {2.3f,4.2f,7.5f};
+        getMaximumFloat(arr);
     }
 
 }
