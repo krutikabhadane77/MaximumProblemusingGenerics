e5552fef2c62c8aa5d4b5bb0646a49cb18960f0d
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-11-01 23:12:59 +0530

    Refactor: all the 3 to One Generic Method and find the maximum

--- src/com/corejavagenerics/TestMaximum.java
+++ src/com/corejavagenerics/TestMaximum.java
@@ -1,25 +1,30 @@
 //UC1 Given 3 Integers find the maximum
 //UC2 Given 3 Floats find the maximum
 //UC3 Given 3 Strings find the maximum
+//Refactor all the 3 to One Generic Method and find the maximum
 package com.corejavagenerics;
 
 public class TestMaximum {
-    public static void getMaximumString(String[] arr) {
-
-        String max = arr[0];
+    public static <T extends Comparable<T>> void getMax(T[] arr) {
+        T max = arr[0];
         int position = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++)
+        {
             int a = arr[i].compareTo(max);
-            if (a > 0) {
+            if(a > 0)
+            {
                 max = arr[i];
                 position = i;
             }
         }
-        System.out.println("Maximum string is " + max+" and its position is "+position);
+        System.out.println("Maximum element is " + max+" and its position is "+position);
     }
     public static void main(String[] args) {
-        String[] arr = {"Apple", "Peach", "Banana"};
-        getMaximumString(arr);
+        Integer[] integerArray = {10, 40, 20};
+        getMax(integerArray);
+        Float[] floatArray = {2.3f,4.2f,7.5f};
+        getMax(floatArray);
+        String[] stringArray = {"Apple", "Peach", "Banana"};
+        getMax(stringArray);
     }
-
 }
